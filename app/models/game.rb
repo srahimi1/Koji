5c789d416400930bc4caec2108c4484bba68ae4e
unknown <salr3467@gmail.com>
2018-03-28 14:37:26 -0400

    new direction in game. made game easier. figure out word and letters after guessing color

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,129 +1,203 @@
 class Game < ApplicationRecord
 
-	def create_colors
+	def start_new_game
+		@game_levels = { a1: {num_colors:3, shades_per_color:3, time_per_letter_segment: 240, min_letters_per_word: 3, max_letters_per_word: 9}, a2:{num_colors:5, shades_per_color:3, time_per_letter_segment: 210, min_letters_per_word: 5, max_letters_per_word: 10}}
+		@level = @game_levels[:a1]		
 		response = {}
-		steps = 9;
-		response_answer = create_answer_color(steps)
-		gradient_answer = response_answer[0]
-		color1_answer_hex = response_answer[1]
-		color2_answer_hex = response_answer[2]
-		other_ends = get_other_ends(response_answer[3],response_answer[4],4)
-		response["answer"] = {}
-		response["answer"]["ends"] = [color1_answer_hex,color2_answer_hex]
-		response["answer"]["selectedColor"] = gradient_answer[Random.new.rand(gradient_answer.length)]
-		response["answer"]["gradient"] = gradient_answer
-		response["otherEnds"] = other_ends
-		response["gradientSteps"] = steps
-		response["mixedUpEnds"] = mix_up(color1_answer_hex, color2_answer_hex, other_ends)
-		response["organizedEnds"] = organize_ends([color1_answer_hex, color2_answer_hex], other_ends)
+		all_colors_hex = []
+		all_colors_rgb = []
+		letters = get_letters
+		two_colors = get_random_colors(2, nil)
+		two_colors_hex = two_colors[0]
+		two_colors_rgb = two_colors[1]
+		in_between_color_hex = get_in_between_color(two_colors_rgb)[0]
+		additional_colors = get_random_colors(@level[:num_colors]-2, two_colors_rgb)
+		additional_colors_hex = additional_colors[0]
+		additional_colors_hex.length.times.map {|i| all_colors_hex.push([additional_colors_hex[i]])}
+		two_colors_hex.length.times do |i|
+			all_colors_hex.push([two_colors_hex[i]])
+		end
+		puts "this is all_colors_hex"
+		puts all_colors_hex.inspect
+		additional_colors_rgb = additional_colors[1]		
+		additional_colors_rgb.length.times.map {|i| all_colors_rgb.push(additional_colors_rgb[i])}
+		two_colors_rgb.length.times do |i|
+			all_colors_rgb.push(two_colors_rgb[i])
+		end
+		puts "test"
+		puts all_colors_rgb.inspect
+		if ( (@level[:shades_per_color] - 1) > 0 )
+			len = all_colors_rgb.length
+			len.times do |i|
+				similar_colors_temp = get_similar_colors(all_colors_rgb[i],@level[:shades_per_color] - 1)
+				similar_colors_temp[0].length.times.map {|j| all_colors_hex[i].push(similar_colors_temp[0][j])}
+			end
+		end
+		puts "tthis is all_colors_rgb after similar colors are added"
+		puts all_colors_rgb.inspect
+		puts "this is all colors hex"
+		puts all_colors_hex.inspect
+		all_colors_hex.length.times do |i|
+			all_colors_hex[i] = order_hex_colors(all_colors_hex[i])
+		end
+		puts "this is all colors hex after sorting"
+		puts all_colors_hex.inspect
+		#in_between_colors_for_similar_colors_hex = create_in_between_colors_for_similar_colors(similar_colors_rgb)
+		response["correctPair"] = two_colors_hex
+		response["correctMixedColor"] = in_between_color_hex
+		response["allColors"] = mix_up(all_colors_hex)
+		response["letters"] = letters
+		#response["spectrumsEnds"] = [in_between_colors_for_similar_colors_hex[2],in_between_colors_for_similar_colors_hex[3]]
+		#response["spectrumsColor"] = in_between_colors_for_similar_colors_hex[1]
+		#response["spectrumsMixes"] = order_hex_colors(in_between_colors_for_similar_colors_hex[0])
 		return response
 	end
 
-	def create_answer_color(steps)
-		color_ends = []
-		2.times do 
-			color_ends.push(get_random_rgb_color)
-		end
-		color1_hex = "#%02X%02X%02X" % [color_ends[0][0],color_ends[0][1],color_ends[0][2]]
-		color2_hex = "#%02X%02X%02X" % [color_ends[1][0],color_ends[1][1],color_ends[1][2]]
-		gradient = create_gradient_from_two_colors(color_ends,steps)
-		gradient.push(color1_hex)
-		gradient.unshift(color2_hex)
-		return [gradient, color1_hex, color2_hex, color_ends[0], color_ends[1]]
+	def get_letters
+		words = ["cat","phone","yellow"]
+		chosen_word = words[rand(words.length)]
+		letters = chosen_word.split("")
+		return letters
 	end
 
-	def get_random_rgb_color
-		component = Random.new
-		color = 3.times.map{component.rand(256)}
+	def get_random_colors(num_of_colors, colors_to_avoid_param)
+		colors_rgb = []
+		colors_hex = []
+		if (!!colors_to_avoid_param && (colors_to_avoid_param.length > 0))
+			colors_to_avoid = colors_to_avoid_param.length.times.map {|i| colors_to_avoid_param[i].clone.length.times.map{|item| item.clone  }  }
+		else
+			colors_to_avoid = [[0,0,0]]
+		end
+		num_of_colors.times do
+			cont = true
+			color_temp = nil
+			while (cont)
+				cont = false
+				color_temp = create_random_rgb_color
+				colors_to_avoid.length.times do |i|
+					if ( (color_temp[0] == colors_to_avoid[i][0]) && (color_temp[1] == colors_to_avoid[i][1]) && (color_temp[2] == colors_to_avoid[i][2]) )
+						cont = true
+						break
+					end 
+				end # end colors_to_avoid.length.times do |i|
+			end # end while
+			colors_rgb.push(color_temp)
+			colors_to_avoid.push(color_temp)
+		end # end num_of_colors.times do 
+		colors_hex = colors_rgb.length.times.map {|i| "#%02X%02X%02X" % [colors_rgb[i][0],colors_rgb[i][1],colors_rgb[i][2]]}
+		return [colors_hex,colors_rgb]
+	end
+
+	def create_random_rgb_color
+		channel = Random.new
+		color = 3.times.map{channel.rand(146)+80}
 		return color
 	end
 
-	def create_gradient_from_two_colors(color_ends, steps)
-		gradient = []
+	def get_in_between_color(color_ends)
 		color1 = color_ends[0]
 		color2 = color_ends[1]
-		alpha = 0.0
-		steps.times do |i|
-			slice = []
-			alpha = alpha + (1.0/steps)
-			slice[0] = color1[0] * alpha + (1-alpha) * color2[0]
-			slice[1] = color1[1] * alpha + (1-alpha) * color2[1]
-			slice[2] = color1[2] * alpha + (1-alpha) * color2[2]
-			hex = "#%02X%02X%02X" % [slice[0],slice[1],slice[2]]
-			gradient.push(hex)
-		end
-		return gradient
-	end
-
-	def shift_color(color, alpha_arg)
 		slice = []
-		rand_num = Random.new.rand(2)
-		alpha = (rand_num == 1) ? (0 - alpha_arg) : alpha_arg
-		factor = 1 + alpha
-		if (  ((factor * color[0]) > 255) || ((factor * color[1]) > 255) || ((factor * color[2]) > 255) )
-			factor = 1 - alpha
-		end
-		slice[0] = factor * color[0]
-		slice[1] = factor * color[1]
-		slice[2] = factor * color[2]
-		color_hex = "#%02X%02X%02X" % [slice[0],slice[1],slice[2]]
-		return color_hex
+		alpha = 0.5
+		slice[0] = color1[0] * alpha + (1-alpha) * color2[0]
+		slice[1] = color1[1] * alpha + (1-alpha) * color2[1]
+		slice[2] = color1[2] * alpha + (1-alpha) * color2[2]
+		hex = "#%02X%02X%02X" % [slice[0],slice[1],slice[2]]
+		return [hex, [slice[0],slice[1],slice[2]]]
 	end
 
-	def get_other_ends(color1, color2, steps)
-		others = []
-		step = 0.0
-		alpha = 1.0/steps
-		steps.times do
-			step = step + alpha  
-			temp = []
-			temp.push(shift_color(color1,step))
-			temp.push(shift_color(color2,step))
-			others.push(temp)
+	def get_similar_colors(color, num_of_colors_in_spectrum)
+		spectrum_hex = []
+		spectrum_rgb = []
+		less_than_80 = false
+		greater_than_255 = false
+		step = 0.15
+		num_of_colors_in_spectrum.times do 
+			random_number = rand(2)
+			if ( (random_number == 0) && (!less_than_80 || greater_than_255) )
+				factor = 1 - step
+			elsif ( (random_number == 1) && !greater_than_255 )
+				factor = 1 + step
+			else
+				factor = 1 - step
+			end
+			less_than_80 = false
+			greater_than_255 = false
+			test1 = (color[0] * factor).to_i
+			test2 = (color[1] * factor).to_i
+			test3 = (color[2] * factor).to_i
+			test1 = (test1 >= 255) ? 255 : test1
+			test2 = (test2 >= 255) ? 255 : test2
+			test3 = (test3 >= 255) ? 255 : test3
+			if ( (test1 < 80) && (test2 < 80) && (test3 < 80) )
+				less_than_80 = true
+			elsif ( (test1 == 255) || (test2 == 255) || (test3 == 255) )
+				greater_than_255 = true
+			end
+			color_hex = "#%02X%02X%02X" % [test1,test2,test3]
+			spectrum_hex.push(color_hex)
+			spectrum_rgb.push([test1,test2,test3])
+			step = step + 0.15
 		end
-		return others
+		return [spectrum_hex,spectrum_rgb]
 	end
 
-	def mix_up(end1, end2, ends_arr)
-		temp_arr = []
-		ends_arr.length.times do |i|
-			temp_arr.push(ends_arr[i][0])
-			temp_arr.push(ends_arr[i][1])
+	def mix_up(colors_orig)
+		puts "this is colors_orig"
+		puts colors_orig.inspect
+		used = {}
+		colors = colors_orig.map {|item| item.clone }
+		mixed_arr = Array.new(colors.length)
+		(mixed_arr.length-((mixed_arr.length/2).floor)).times do |i|
+			popped = colors.pop
+			ind = rand(mixed_arr.length)
+			while (mixed_arr[ind] != nil)
+				ind = rand(mixed_arr.length)
+			end
+			mixed_arr[ind] = popped
 		end
-		temp_arr.push(end1)
-		temp_arr.push(end2)
-		ind_hash = {}
-		mixed_arr = []
-		temp_arr.length.times do |i|
-			ind_hash[i] = 1
-		end
-		random_number_maker = Random.new
-		len = ind_hash.length
-		while (len > 0) do
-			keys_arr = ind_hash.keys
-			random_number = random_number_maker.rand(keys_arr.length)
-			mixed_arr.push(temp_arr[keys_arr[random_number].to_i])
-			ind_hash.delete(keys_arr[random_number])
-			len = ind_hash.length
-		end
-		mixed_arr.sort_by! {|color| color.downcase }
+		2.times do
+			popped = colors.pop
+			mixed_arr.length.times do |i|
+				if (!mixed_arr[i])
+					mixed_arr[i] = popped
+					break
+				end # end if
+			end # end mixed_arr.length.times
+		end # end 2.times do
+		puts "this is mixed_arr"
+		puts mixed_arr.inspect
 		return mixed_arr
 	end
 
-	def organize_ends(color_ans, other)
+	def order_hex_colors(colors_arr)
 		arr = []
-		other[0].length.times do |i|
-			temp_arr = []
-			other.length.times do |j|
-				temp_arr.push(other[j][i])
-			end
-			temp_arr.push(color_ans[i])
-			temp_arr.sort_by! {|color| color.downcase}
-			temp_arr.length.times do |j|
-				arr.push(temp_arr[j])
+		arr = colors_arr.map {|color| color.clone}
+		arr.sort_by! {|color| color.downcase}
+		return arr
+	end
+
+	def create_in_between_colors_for_similar_colors(spects)
+		puts spects.inspect
+		arr = []
+		ans = ""
+		col1 = ""
+		col2 = ""
+		counter = 0
+		ans_ind = rand(spects[0].length * spects[1].length)
+		spects[0].length.times do |i|
+			spects[1].length.times do |j|
+				col = get_in_between_color([spects[0][i], spects[1][j]])[0]
+				arr.push(col)
+				if (ans_ind == counter)
+					ans = col
+					col1 = "#%02X%02X%02X" % [spects[0][i][0],spects[0][i][1],spects[0][i][2]]
+					col2 = "#%02X%02X%02X" % [spects[1][j][0],spects[1][j][1],spects[1][j][2]]
+				end
+				counter = counter + 1
 			end
 		end
-		return arr
+		return [arr,ans,col1,col2]
 	end
 
 end
