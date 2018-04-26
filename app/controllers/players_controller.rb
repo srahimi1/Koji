c3a09ee23dce1d0c21b46ac2d6fee6914d6cfee2
unknown <salr3467@gmail.com>
2018-04-26 14:49:00 -0400

    testing action mailer

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,7 +7,7 @@ class PlayersController < ApplicationController
 			player = Player.new(email: params["email"], cellphone: params["cellphone"], display_name: params["display_name"], phone_country: "USA", game_version: params["game_version"], subscribed: 0, email_verified: 0, cellphone_verified: 0)
 			if player.save
 				puts "player saved"
-				PlayerMailer.with(cellphone: player.cellphone, code: "123BB").send_cellphone_confirmation.deliver
+				PlayerMailer.send_cellphone_confirmation(player.cellphone, "1234zz").deliver_now
 			end
 		end
 	end
--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -1,11 +1,8 @@
 class PlayerMailer < ApplicationMailer
 
-	def send_cellphone_confirmation
-		num = params["cellphone"]
-		code = params["code"]
+	def send_cellphone_confirmation(num, code)
 		add = num.to_s+"@vtext.com"
 		message = "your koji confirmation code is "+code.to_s
 		mail(from: "kojigame.com", to: add, subject: "koji confirmation code", body: message)
-		end
 	end
 end
