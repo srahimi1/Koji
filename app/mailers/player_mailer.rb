d2fc529948be6c5d970e6c387c0df92782480e1c
unknown <salr3467@gmail.com>
2018-04-26 16:30:36 -0400

    testing mailer

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -5,6 +5,6 @@ class PlayerMailer < ApplicationMailer
 		puts "this is the address"
 		puts add
 		message = "your koji confirmation code is "+code.to_s
-		mail(from: "kojigame.com", to: add, subject: "koji confirmation code", body: message)
+		mail(from: "kojigame.com", to: add, subject: "", body: message)
 	end
 end
