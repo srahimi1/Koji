0e7099ebf08cfd874526b610cede7de0c2803bed
unknown <salr3467@gmail.com>
2018-04-27 10:44:12 -0400

    test maiL

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -3,7 +3,7 @@ class PlayerMailer < ApplicationMailer
 	def send_confirmation_text(num, code, provider)
 		add = num.to_s+"@"+provider
 		message = "Your Koji confirmation code is "+code.to_s
-		mail(from: "", subject: "", to: add, body: message)
+		mail(from: "Koji game", subject: "", to: add, body: message)
 	end
 
 	def send_confirmation_email(email, code)
