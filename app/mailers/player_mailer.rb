d2a63f46935d49a6bea192bcc86265309d3f7372
unknown <salr3467@gmail.com>
2018-05-01 13:51:22 -0400

    testing texter

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -3,11 +3,11 @@ class PlayerMailer < ApplicationMailer
 	def send_confirmation_text(num, code, provider)
 		@code = code
 		add = num.to_s+"@"+provider
-		mail(from: "sendgrid.net", subject: "", to: add)
+		mail(from: "sendgrid.me", subject: "", to: add)
 	end
 
 	def send_confirmation_email(email, code)
 		@code = code
-		mail(from: "sendgrid.net", to: email, subject: "kojigame confirmation code")
+		mail(from: "sendgrid.me", to: email, subject: "kojigame confirmation code")
 	end
 end
