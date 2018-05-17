d62e60042897bdf95202b68fd55f80dd3ddaa4ef
unknown <salr3467@gmail.com>
2018-05-17 12:42:48 -0400

    lower frequency of cycling through colors and change colors of menu fonts

--- app/models/confirmation_code.rb
+++ app/models/confirmation_code.rb
@@ -24,12 +24,12 @@ class ConfirmationCode < ApplicationRecord
   		end
   		cc = "#{cc}#{char}"
   	end
-  	player_cc = ConfirmationCode.find_by(player_id: pid, method: mthd)
-  	if (!player_cc) 
-  		player_cc = ConfirmationCode.create(player_id: pid, code: cc, method: mthd)
+  	player_cc = ConfirmationCode.find_by(player_id: pid, bymethod: mthd)
+  	if (!player_cc.blank?) 
+  		player_cc = ConfirmationCode.create(player_id: pid, code: cc, bymethod: mthd)
   	else 
   		player_cc.code = cc
-      player_cc["method"] = mthd
+      player_cc.bymethod = mthd
   	end
   	player_cc.save
   	return cc
