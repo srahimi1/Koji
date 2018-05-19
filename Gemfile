5ceedb1b7306fcadfaeeabc25a02fda58683ff84
unknown <salr3467@gmail.com>
2018-05-19 12:41:19 -0400

    integrating stripe with appropraite environment variables

--- Gemfile
+++ Gemfile
@@ -30,7 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+# Stripe.com gem for charges and subscriptions
+gem 'stripe'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,22 @@
 class Subscription < ApplicationRecord
   belongs_to :player
   has_one :payment_provider, dependent: :destroy
+
+  require "stripe"
+
+  def self.subscription_enroll(token_id, token_email)
+
+  	customer = Stripe::Customer.create(
+  		:email => token_email,
+  		:source => token_id
+  	)
+
+  	subscription = Stripe::Subscription.create(
+  		:customer => customer.id,
+  		:items => [{plan: 'plan_CscHlXCt8cGalL'}]
+  	)
+
+  end
+
+
 end
