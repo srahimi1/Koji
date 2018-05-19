3e6315e11d3279df845a4dccdb339eed33bc54ea
unknown <salr3467@gmail.com>
2018-05-19 13:10:00 -0400

    fixing stripe

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,6 +5,24 @@ class Subscription < ApplicationRecord
   require "stripe"
 
   def self.subscription_enroll(token_id, token_email)
+	puts " "
+	puts " "
+	puts " "
+  	puts " "
+  	puts "this is token id"
+  	puts token_id
+  	puts token_id.to_s
+  	puts token_id.inspect
+  	puts " "
+  	puts " "
+  	puts " "
+  	puts " "
+  	puts token_email
+  	puts token_email.to_s
+  	puts token_email.inspect
+
+
+
 
   	customer = Stripe::Customer.create(
   		:email => token_email,
