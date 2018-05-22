20f3b242ff0250c450204e1830e2ea06391b59b6
unknown <salr3467@gmail.com>
2018-05-22 00:33:37 -0400

    debugging password reset done for now

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -3,7 +3,7 @@ class PlayerMailer < ApplicationMailer
 	def send_confirmation_text(num, code, provider)
 		@code = code
 		add = num.to_s+"@"+provider
-		body = "Your Koji cellphone confirmation code is: #{@code}"
+		body = "Your Koji reset password confirmation code is: #{@code}"
 		mail(from: "kojigame@sendgrid.net", subject: "", to: add, body: body, content_type: "text/plain")
 	end
 
