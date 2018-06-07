0930011a15bcbd1a78b205d5e7d53b2e645f29ef
unknown <salr3467@gmail.com>
2018-06-07 16:12:35 -0400

    removed foreign key constraints for subscriptions and ppcustimerinfo

--- db/migrate/20180426104339_create_subscriptions.rb
+++ db/migrate/20180426104339_create_subscriptions.rb
@@ -1,13 +1,13 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.1]
   def change
     create_table :subscriptions do |t|
-      t.references :player, foreign_key: true
+      t.references :player
       t.timestamp :date_first_subscribed
       t.string :pp_subscription_id
       t.integer :status
       t.string :status_description
       t.timestamp :date_last_charged
-      t.references :payment_provider, foreign_key: true
+      t.references :payment_provider
       t.string :pp_player_id
       t.string :most_recent_transaction_confirmation_number
       t.text :payment_history
--- db/migrate/20180426104445_create_pp_customer_infos.rb
+++ db/migrate/20180426104445_create_pp_customer_infos.rb
@@ -1,8 +1,8 @@
 class CreatePpCustomerInfos < ActiveRecord::Migration[5.1]
   def change
     create_table :pp_customer_infos do |t|
-      t.references :player, foreign_key: true
-      t.references :payment_provider, foreign_key: true
+      t.references :player
+      t.references :payment_provider
       t.string :pp_customer_id
 
       t.timestamps
