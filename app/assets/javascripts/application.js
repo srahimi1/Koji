fb83b751a4d7cc24ccdff76c95ebd9c81d4cac8f
unknown <salr3467@gmail.com>
2018-06-24 07:28:45 -0400

    still fixing goal resizing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,7 +48,7 @@ previousMinute = 0;
 xhttp = new XMLHttpRequest();
 counterU = 0;
 correctLetters = [];
-canvas = null, ctx = null, gc = null;
+canvas = null, ctx = null, gc = null, gcheight = 0;
 imageData = null;
 similarLettersLowerCase = {"a": "egqdDQGB", "b": "hdgopqPFLK", "c": "eouvhyQEDO", "d": "bpqghPRBD", "e": "agqdEFKRP", "f": "ktjiFLKR", "g":"abdopqGQO", "h":"bdkrvHLRK", "i":"ljtILJT", "j":"iltTKRL", "k":"bdepxMWFE", "l": "ijtLPKH", "m": "nwuveWNRHE", "n": "muwvWRMK", "o":"bqpcdQBP", "p": "bdgceBRDE", "q": "pbdeBPDR", "r":"nuhHJLK", "s":"czg", "t": "ijlfk", "u": "vnyhc", "v": "unyc", "w": "mnhuvEM", "x": "kwmyz", "y": "zvukh", "z":"snum"};
 similarLettersUpperCase = {"A": "VYUHegqd", "B": "KEPRFXhdgopq", "C": "GOQDeouvhy", "D": "CGOQbpqgh", "E": "KBPRFXMagqd", "F" : "KBEPRXktji", "G":"COQDabdopq", "H":"ITLJAbdkrv", "I":"HTLJljt", "J":"HITLilt", "K":"BEPRFXbdepx", "L":"HITJijt", "M":"NWUHEnwuv", "N": "MWUHEmuwv", "O": "CGQDbqpcd", "P": "KBERFXbdgce", "Q":"GCDOpbde", "R": "KBEPFXnuh", "S" : "ZCBE", "T": "HILJ", "U": "AVYH", "V" : "AYUN", "W" : "YMNEm", "X" : "BEKZS", "Y":"VUNH", "Z" : "SNMUXK"};
@@ -88,14 +88,15 @@ function removeButtonColorOnTouchEnd(element) {
 
 function expandGoalContainer() {
 	var body = document.getElementsByTagName("body")[0];
+	gcheight = gc.offsetHeight;
 	gc.parentNode.removeChild(gc);
 	gc.style.position = "absolute";
 	var pandx = document.getElementById("pointsandxsContainer");
 	var totalht = document.getElementById("KOJITitle").offsetHeight + pandx.offsetHeight;
 	gc.style.top = totalht + "px";
 	body.appendChild(gc);
-	canvas.style.height = canvas.offsetHeight + "px";
-	document.getElementById("goal").style.height = canvas.offsetHeight + "px";
+	canvas.style.height = gcheight + "px";
+	document.getElementById("goal").style.height = gcheight + "px";
 	gc.style.marginTop = (0 - pandx.offsetHeight) + "px";
 	gc.style.height = document.getElementById('gameContent').offsetHeight + "px";
 	gc.onclick = function() {shrinkGoalContainer();};
@@ -104,9 +105,9 @@ function expandGoalContainer() {
 function shrinkGoalContainer() {
 	var body = document.getElementsByTagName("body")[0];
 	var go = document.getElementById("guessOuter");
-	go.style.height = canvas.offsetHeight + "px";
+	go.style.height = gcheight + "px";
 	gc.style.marginTop = "0px";
-	gc.style.height = canvas.offsetHeight + "px";
+	gc.style.height = gcheight + "px";
 	setTimeout(function() {gc.parentNode.removeChild(gc); gc.style.position = "relative"; gc.style.top = "0px"; go.parentNode.insertBefore(gc,go.previousSibling.previousSibling);}, 1650);
 } // end function shrinkGoalContainer()
 
