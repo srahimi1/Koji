745f1e42a4805019c16d5e96a2964de492f8de77
unknown <salr3467@gmail.com>
2018-07-09 17:45:01 -0400

    testing webworker

--- public/timer.js
+++ public/timer.js
@@ -1,13 +1,13 @@
-timeoutID = null, dateStart = new Date(), start = dateStart.getTime(), time = 0;
+timeoutID = null, dateStart = new Date(), start = dateStart.getTime(), time = -100;
 
 function getDateTime() {
 	time += 100;
 	var n = new Date();
 	v = n.getTime();
 	postMessage(v);
-	var diff = (new Date.getTime()) - start - time;
+	var diff = v - start - time;
 	clearTimeout(timeoutID);
-	timeoutID = setTimeout(getDateTime,100 - diff);
+	timeoutID = setTimeout(function() {getDateTime()},100 - diff);
 } // end function getDateTime()
 
 getDateTime();
