6866d65a12d87f6a95f6ed823eddb8fd98809f54
unknown <salr3467@gmail.com>
2018-07-22 10:02:45 -0400

    changing some wording in walk-through instructions and timing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -980,7 +980,7 @@ function showDemoInstructions() {
 	setTimeout(function() {document.getElementById("demoInstructions7").style.opacity = 1;}, 25000);
 	setTimeout(function() {drawLine(); drawLine();}, 28000);
 	setTimeout(function() {document.getElementById("demoInstructions8").style.opacity = 1;}, 29000);
-	setTimeout(function() {numberCorrect = 6; selectUnderscore(document.getElementById("letterBox:0"), inputData.letters); switchButtonsAndLetters(2);}, 33000);
+	setTimeout(function() {numberCorrect = 6; selectUnderscore(document.getElementById("letterBox:0"), inputData.letters); switchButtonsAndLetters(2);}, 34000);
 	setTimeout(function() {selectUnderscore(document.getElementById("letterBox:1"), inputData.letters); }, 35000);
 	setTimeout(function() {selectUnderscore(document.getElementById("letterBox:2"), inputData.letters); }, 36000);
 	setTimeout(function() {selectUnderscore(document.getElementById("letterBox:1"), inputData.letters); }, 37000);
