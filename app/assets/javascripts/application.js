eda387538758e6c45da96f5bc91728c37aaeddb3
unknown <salr3467@gmail.com>
2018-07-23 16:57:48 -0400

    reverted back to previous code, removed change o=f display mode of buttonsDiv

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1839,7 +1839,6 @@ function isSignupFormReady() {
 // general purpose app functions ...
 
 function showMenu(el) {
-	document.getElementById("buttonsDiv").style.display = "none";
 	var id = el.id + "";
 	if (id == "signupDiv") {
 		document.getElementById("signupForm").reset();
@@ -1863,7 +1862,6 @@ function showMenu(el) {
 } // end function showMenu(el)
 
 function closeMenu(el) {
-	document.getElementById("buttonsDiv").style.display = "flex";
 	el.style.opacity = "0";
 	el.style.marginTop = "-2em";
 	setTimeout(function() {el.style.display = "none";}, 800);
