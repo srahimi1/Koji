ceb814b705aca32c7f75201ad166e9e9c40783c1
unknown <salr3467@gmail.com>
2018-08-01 22:13:42 -0400

    testing header access-control-allow-origin: '*' in games controller to see if it can bypass CORS policy 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  after_filter :set_access
-
-  def set_access
-    @response.headers['Access-Control-Allow-Origin'] = "*"
-  end
 end
