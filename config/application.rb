94a5f8de650b73f961de46e68aea324f1f586244
unknown <salr3467@gmail.com>
2018-08-03 18:25:09 -0400

    a

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module App1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.action_dispatch.default_headers = {"Access-Control-Allow-Origin" => "*"}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
