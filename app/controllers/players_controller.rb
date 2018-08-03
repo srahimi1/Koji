8f3750f784b9d37e613d03f0c285bff323ac0567
unknown <salr3467@gmail.com>
2018-08-03 18:02:54 -0400

    A

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -164,6 +164,10 @@ class PlayersController < ApplicationController
 	end
 
 	def startup_message
+		puts "this is"
+		puts " "
+		puts " "
+		puts "1:q:" + form_authenticity_token
 		render plain: "1:q:" + form_authenticity_token 
 	end
 
