a47f61de8223d9d3a48e8efd6d5e7c6c1373e942
unknown <salr3467@gmail.com>
2018-08-04 10:19:01 -0400

    a

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module App1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_disptach.default_headers = {
+    	"Access-Control-Allow-Origin" => "*",
+      	"Access-Control-Allow-Methods" => "OPTIONS, GET, POST, PATCH, PUT, DELETE",
+      	"Access-Control-Allow-Headers" => "X-PINGOTHER, Content-Type",
+      	"Access-Control-Max-Age" => "86400"
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
