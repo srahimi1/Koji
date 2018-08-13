a80d32bfa34bce1438cb82322917e4fd10c2b046
unknown <salr3467@gmail.com>
2018-08-13 17:35:10 -0400

    a

--- app/controllers/google_play_subscriptions_controller.rb
+++ app/controllers/google_play_subscriptions_controller.rb
@@ -4,7 +4,7 @@ class GooglePlaySubscriptionsController < ApplicationController
 
 
 	def get_access_and_refresh_tokens
-		params2 = {grant_type: authorization_code, code: params[:code], client_id: "29709825761-lg7lau2323objqt0lrg5mikohjbcjijl.apps.googleusercontent.com", client_secret: "wOc9rMIMupQXDkyPd6iXg_ur", redirect_uri: "http://arsr-app1.herokuapp.com/googleplaysubscriptions/getAccessAndRefreshTokens"}
+		params2 = {grant_type: "authorization_code", code: params[:code], client_id: "29709825761-lg7lau2323objqt0lrg5mikohjbcjijl.apps.googleusercontent.com", client_secret: "wOc9rMIMupQXDkyPd6iXg_ur", redirect_uri: "http://arsr-app1.herokuapp.com/googleplaysubscriptions/getAccessAndRefreshTokens"}
 		x = Net::HTTP.post_form(URI.parse("https://accounts.google.com/o/oauth2/token"),params2)
 		render plain: x.body.inspect
 	end
