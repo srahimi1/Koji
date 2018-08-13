b8bad38fd770725a0dcba1cd3e5704f074f75958
unknown <salr3467@gmail.com>
2018-08-13 18:52:56 -0400

    a

--- app/controllers/google_play_subscriptions_controller.rb
+++ app/controllers/google_play_subscriptions_controller.rb
@@ -4,18 +4,27 @@ class GooglePlaySubscriptionsController < ApplicationController
 
 
 	def get_access_and_refresh_tokens
-		puts params.inspect
-		puts " code is   "
-		puts " "
-		puts params["code"]
-		puts " "
-		puts " that was it "
-		params2 = {grant_type: "authorization_code", code: params["code"].to_s, client_id: "29709825761-lg7lau2323objqt0lrg5mikohjbcjijl.apps.googleusercontent.com", client_secret: "wOc9rMIMupQXDkyPd6iXg_ur", redirect_uri: "http://arsr-app1.herokuapp.com/googleplaysubscriptions/getAccessAndRefreshTokens"}
+		params2 = {grant_type: "authorization_code", code: params["code"].to_s, client_id: ENV["GOOGLE_PLAY_CLIENT_ID"], client_secret: ENV["GOOGLE_PLAY_CLIENT_SECRET"], redirect_uri: "http://arsr-app1.herokuapp.com/googleplaysubscriptions/getAccessAndRefreshTokens"}
 		x = Net::HTTP.post_form(URI.parse("https://accounts.google.com/o/oauth2/token"),params2)
-		render plain: x.body.inspect
+		puts "this is the refre toke"
+		puts " "
+		puts x.body.refresh_token
+		puts " "
+		puts " "
+		render plain: x.body.refresh_token
 	end
 
+	
+	def get_access_token_from_refresh_token
+		params2 = {grant_type: "refresh_token", client_id: ENV["GOOGLE_PLAY_CLIENT_ID"], client_secret: ENV["GOOGLE_PLAY_CLIENT_SECRET"], refresh_token: "http://arsr-app1.herokuapp.com/googleplaysubscriptions/getAccessAndRefreshTokens"}
+		x = Net::HTTP.post_form(URI.parse("https://accounts.google.com/o/oauth2/token"),params2)
+	end
+
+
+
+
 	def subscribe_with_google_play
+		at = get_access_token_from_refresh_token
 		render plain: "IT made it!!!!!!"
 	end
 
