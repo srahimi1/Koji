4e8c0784c40c1f88d53b6e92f5fca164144e1b63
unknown <salr3467@gmail.com>
2018-08-22 01:35:43 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,31 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  resources :players
-  resources :paymentserviceproviders
-  resources :games, :only => [:create, :update]
-  resources :messages, only: [:create]
+  scope "/koji/v1/game/dataspace" do
 
-  get '/index1', to: 'welcome#index1'
-  get '/index2', to: 'welcome#index2'
-  get '/index3', to: 'welcome#index3'
-  get '/index4', to: 'welcome#index4'
-  get '/index5', to: 'welcome#index5'
-  get '/index6', to: 'welcome#index6'
-  get '/all', to: 'welcome#all'
+    resources :players
+    resources :paymentserviceproviders
+    resources :games, :only => [:create, :update]
+    resources :messages, only: [:create]
+
+    get '/index1', to: 'welcome#index1'
+    get '/index2', to: 'welcome#index2'
+    get '/index3', to: 'welcome#index3'
+    get '/index4', to: 'welcome#index4'
+    get '/index5', to: 'welcome#index5'
+    get '/index6', to: 'welcome#index6'
+    get '/all', to: 'welcome#all'
 
 
 
-  get '/checkemail', to: 'players#check_email'
-  get '/checkcellphone', to: 'players#check_cellphone'
-  get '/checkdisplayname', to: 'players#check_displayname'
-  get '/message', to: 'players#startup_message'
-  post '/players/login', to: 'players#login'
-  get '/googleplaysubscriptions/subscribeWithGooglePlay', to: 'google_play_subscriptions#subscribe_with_google_play'
-  get '/googleplaysubscriptions/getAccessAndRefreshTokens', to: 'google_play_subscriptions#get_access_and_refresh_tokens'
-  get '/googleplaysubscriptions/getAccessTokenFromRefreshToken', to: 'google_play_subscriptions#get_access_token_from_refresh_token'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/checkemail', to: 'players#check_email'
+    get '/checkcellphone', to: 'players#check_cellphone'
+    get '/checkdisplayname', to: 'players#check_displayname'
+    get '/message', to: 'players#startup_message'
+    post '/players/login', to: 'players#login'
+    get '/googleplaysubscriptions/subscribeWithGooglePlay', to: 'google_play_subscriptions#subscribe_with_google_play'
+    get '/googleplaysubscriptions/getAccessAndRefreshTokens', to: 'google_play_subscriptions#get_access_and_refresh_tokens'
+    get '/googleplaysubscriptions/getAccessTokenFromRefreshToken', to: 'google_play_subscriptions#get_access_token_from_refresh_token'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
