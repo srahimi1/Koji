2374cbd14c3d56584e06ecf824a7e98593d7685e
unknown <salr3467@gmail.com>
2018-08-23 18:45:36 -0400

    Full release V.1 - renamed app packageName and folder

---
+++ Koji/platforms/android/app/build/generated/source/buildConfig/release/koji/koji/koji/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package koji.koji.koji;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "koji.koji.koji";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 11001;
+  public static final String VERSION_NAME = "1.1.1";
+}
