1d56ccbd06857c1a678e3472967992b5e30253d3
unknown <salr3467@gmail.com>
2018-08-29 08:00:12 -0400

    Full release v 11023 1.0.23 - reduced font size in demo instructions from 1.7em to 1.4em

--- Koji/platforms/android/app/build/generated/source/buildConfig/release/koji/koji/koji/BuildConfig.java
+++ Koji/platforms/android/app/build/generated/source/buildConfig/release/koji/koji/koji/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "koji.koji.koji";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 11022;
-  public static final String VERSION_NAME = "1.0.22";
+  public static final int VERSION_CODE = 11023;
+  public static final String VERSION_NAME = "1.0.23";
 }
