05b8c473d70b43d67be9f0eef5178a9d2968c78d
unknown <salr3467@gmail.com>
2018-09-02 23:44:03 -0400

    Full release v 11038 1.0.38 - added show definition button to letters screen

--- Koji/platforms/android/app/build/generated/source/buildConfig/release/koji/koji/koji/BuildConfig.java
+++ Koji/platforms/android/app/build/generated/source/buildConfig/release/koji/koji/koji/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "koji.koji.koji";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 11037;
-  public static final String VERSION_NAME = "1.0.37";
+  public static final int VERSION_CODE = 11038;
+  public static final String VERSION_NAME = "1.0.38";
 }
